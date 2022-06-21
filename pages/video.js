9ea691d3e0f05cb6cade4744f9e17f5cb5133645
Gopinath-Selvam <gopinaths16@protonmail.com>
2022-06-21 11:43:32 +0530

    ngrok

--- pages/video.js
+++ pages/video.js
@@ -89,7 +89,7 @@ function Video() {
 
         setIsDisabled(true)
 
-        const socket = io("https://0cb2-2401-4900-3607-6da2-2016-841a-d51-d618.in.ngrok.io")
+        const socket = io("https://750a-2401-4900-3609-a916-d19-4ce5-3dd5-2f67.in.ngrok.io")
 
         console.log(peerID);
         socket.emit("create-room", roomID.current, peerID)
@@ -134,7 +134,7 @@ function Video() {
         event.preventDefault()
         setIsDisabled(true)
 
-        const socket = io("https://0cb2-2401-4900-3607-6da2-2016-841a-d51-d618.in.ngrok.io")
+        const socket = io("https://750a-2401-4900-3609-a916-d19-4ce5-3dd5-2f67.in.ngrok.io")
 
         console.log(peerID);
 
